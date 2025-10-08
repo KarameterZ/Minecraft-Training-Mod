2d2881e93c9a5a7fcd3024b6cb3d423866769eb9
KarameterZ <karameterz@gmail.com>
2025-10-08 16:08:22 +1100

    Add Clinet

---
+++ src/main/java/net/karameterz/training/TrainingClient.java
@@ -0,0 +1,10 @@
+package net.karameterz.training;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrainingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
